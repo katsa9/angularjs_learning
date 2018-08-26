7c9ed3a5ca1fefe4d471e62933b448e7eec2116d
katsa9 <kathleen.brand2@gmail.com>
2018-08-26 13:54:26 +0200

    added a factory

--- ngClassifieds/components/classifieds.ctr.js
+++ ngClassifieds/components/classifieds.ctr.js
@@ -3,8 +3,8 @@
 
 angular
     .module("ngClassifieds")
-    .controller("classifiedsCtrl", function($scope, $http) {
-        $http.get('data/classifieds.json').then(function(data){
+    .controller("classifiedsCtrl", function($scope, $http, classifiedsFactory) {
+        classifiedsFactory.getClassifieds().then(function(data){
             $scope.classifieds = data.data;
         });
     });
